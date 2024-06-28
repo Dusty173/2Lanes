ea7855e977d1654c4c24565ce3d2f0f38a6afb55
Dustin <dustincheyney@gmail.com>
2024-06-28 11:07:50 -0700

    posts model update

--- backend/models/posts.js
+++ backend/models/posts.js
@@ -3,17 +3,17 @@ const { NotFoundError } = require("../expressError");
 const { sqlForPartialUpdate } = require("../helpers/updateSql");
 
 class Post {
-  static async getPosts() {
-    let res =
-      await db.query(`SELECT id, title, body, user_id, created_at FROM posts \
-        ORDER BY created_at DESC`);
+  static async getAll() {
+    let res = await db.query(
+      `SELECT id, title, body, user_id, created_at FROM posts6`
+    );
 
     if (!res) throw new NotFoundError("Unable to retrieve Posts");
 
     return res.rows;
   }
 
-  static async createPost(data) {
+  static async create(data) {
     const res = await db.query(
       `INSERT INTO posts (title, body, user_id) 
         VALUES ($1, $2, $3) RETURNING title, body, user_id, created_at`,
@@ -25,15 +25,32 @@ class Post {
     return post;
   }
 
-  static async removePost(id, userId) {
+  static async remove(data) {
     const res = await db.query(
       `DELETE FROM posts WHERE id = $1 AND user_id = $2 RETURNING title`,
-      [id, userId]
+      [data.id, data.userId]
     );
 
     let deleted = res.rows[0];
     if (!deleted) throw new NotFoundError("Unable to find post to remove");
 
     return deleted;
+  }
+
+  static async update(title, data) {
+    const { setCols, values } = sqlForPartialUpdate(data, {
+      title: "title",
+      body: "body",
+    });
+    const handleIdx = "$" + (values.length + 1);
+
+    const querySql = `UPDATE posts SET ${setCols} WHERE title = ${handleIdx} RETURNING title, body, created_at`;
+
+    const res = await db.query(querySql, [...values, title]);
+    const post = res.rows[0];
+
+    if (!post) throw new NotFoundError(`Post ${title} does not exist.`);
+
+    return post;
   }
 }
